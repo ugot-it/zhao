86c75ffe1e6f3e9b5cba8c4eec0e4a621ddb4a73
周琬怡 <1508482709@qq.com>
2026-06-10 23:41:51 +0800

    更新

---
+++ zhao/demo/src/main/java/com/entity/UsersEntity.java
@@ -0,0 +1,72 @@
+package com.entity;
+import java.io.Serializable;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+
+/**
+ * @author 朝
+ * @ClassName: UsersEntity
+ * @Description: 用户
+ * @date 2026-05-20  下午 3:04
+ * @Version:
+ */
+
+
+@TableName("users")
+public class UsersEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+
+    // 用户类型
+    private String role;
+
+    private Date addtime;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Date addtime) {
+        this.addtime = addtime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
