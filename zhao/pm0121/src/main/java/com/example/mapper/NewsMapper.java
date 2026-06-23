85b1132f33ff4d544b12dc56623235e9709dd41a
赵佳琬_2340129250 <2893204197@qq.com>
2026-06-23 17:59:54 +0800

    删掉传错的文件

--- zhao/pm0121/src/main/java/com/example/mapper/NewsMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.News;
-
-public interface NewsMapper extends BaseMapper<News> {
-
-}
--- zhao/pm0121/src/main/java/com/example/mapper/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.Product;
-
-public interface ProductMapper extends BaseMapper<Product>{
-
-
-}
