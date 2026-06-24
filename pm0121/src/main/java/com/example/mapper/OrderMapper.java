1c3cef40d4c796571e3c8c7aae03d4ad425759cc
ZhaoShadow <241610393@qq.com>
2026-06-24 12:37:44 +0800

    Delete pm0121 directory

--- pm0121/src/main/java/com/example/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.Order;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderMapper extends BaseMapper<Order> {
-    // 空接口即可获得基础CRUD功能
-}
--- pm0121/src/main/java/com/example/pojo/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.pojo;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
